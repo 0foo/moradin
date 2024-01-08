01626b9266cd7d4646b8a86131c96cae6636c886
0foo <0foo@0foo.dev>
2024-01-08 10:42:19 -0600

    update

--- .geraldine/custom_filters/custom_jinja_filters.py
+++ .geraldine/custom_filters/custom_jinja_filters.py
@@ -31,6 +31,12 @@ def only_keys(d, keys):
     #     raise (f"without_keys custom filters was passed a variable that's not dict {d}!!")
     return d
 
+# returns the top level keys of the dict passed 
+def top_level_keys(value):
+    if isinstance(value, dict):
+        return list(value.keys())
+    return value
+
 def format_as_item_href(value):
     # Lowercase the string and replace spaces with dashes
     formatted_value = value.lower().replace(" ", "-")
@@ -40,5 +46,15 @@ def format_as_item_href(value):
 
     return href
 
+def type_tree(obj):
+    if isinstance(obj, dict):
+        # Handle dictionaries by replacing each value with its type or a recursive call
+        return {k: type_tree(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        # Handle lists by applying the same logic to each element
+        return [type_tree(item) for item in obj if not (isinstance(item , str) or isinstance(item, int))]
+    else:
+        # For all other types, return the type as a string
+        return str(type(obj).__name__)
 
 
